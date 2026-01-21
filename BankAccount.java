a64004c00a5e7a9c7705f5c0ba67d596d02ca9ac
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-01-21 14:44:09 +0530

    Add abstract BankAccount class with encapsulation

---
+++ BankAccount.java
@@ -0,0 +1,24 @@
+abstract class BankAccount{
+    private int accountNumber;
+    private String name;
+    private double balance;
+    
+    public BankAccount(int accountNumber,String name){
+        this.accountNumber=accountNumber;
+        this.name=name;
+        this.balance=0.0;
+    }
+    public void deposit(double amount){
+        if(amount>0){
+            balance=balance+amount;
+            System.out.println("Invalid amount");
+        }
+    }
+    public double getBalance(){
+        return balance;
+    }
+    public int getAccountNumber(){
+        return accountNumber;
+    }
+    public abstract void withdraw(double amount);
+}
